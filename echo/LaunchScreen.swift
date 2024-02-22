0eb05da0ff091dbfe36e2b6592bd52a662e7b1e9
Andy <alimablanca@gmail.com>
2024-02-22 09:49:10 -0600

    Segundo commit

--- echo/LaunchScreen.swift
+++ echo/LaunchScreen.swift
@@ -7,12 +7,48 @@
 
 import SwiftUI
 
-struct LaunchScreen: View {
+
+struct GradientLeaf: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "leaf.fill")
+            .foregroundColor(.white)
+            .font(.system(size: 200))
+            .frame(width: 60, height: 100)
     }
 }
 
-#Preview {
-    LaunchScreen()
+
+struct LaunchScreen: View {
+    @State private var isFalling = false
+
+    
+    let startingPoints: [CGPoint] = [
+        CGPoint(x: 100, y: -100),
+        CGPoint(x: 300, y: 200),
+        CGPoint(x: 100, y: 400)
+    ]
+
+    var body: some View {
+        ZStack {
+            Color("ColorPrincipal")
+                .edgesIgnoringSafeArea(.all)
+            ForEach(0..<3) { index in
+                GradientLeaf()
+                    .rotationEffect(.degrees(isFalling ? 0 : 360))
+                    .position(x: startingPoints[index].x, y: isFalling ? UIScreen.main.bounds.height + 200 : startingPoints[index].y)
+                    .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false), value: isFalling)
+            }
+        }
+        .onAppear {
+            isFalling = true
+        }
+    }
+}
+
+
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
 }
--- echo/Preview Content/LoginView.swift
+++ echo/Preview Content/LoginView.swift
@@ -1,18 +1,109 @@
 //
-//  LoginView.swift
-//  echo
+//  LogInView.swift
+//  echoChallenges
 //
 //  Created by Andrea Lima Blanca on 22/02/24.
 //
 
 import SwiftUI
 
-struct LoginView: View {
+struct PasswordTextField: View {
+    @Binding var text: String
+    @Binding var isSecure: Bool
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .trailing) {
+            if isSecure {
+                SecureField("Contraseña", text: $text)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color("ColorPrincipal").opacity(0.20))
+                    .cornerRadius(20)
+            } else {
+                TextField("Password", text: $text)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color("ColorPrincipal").opacity(0.2))
+                    .cornerRadius(20)
+            }
+            
+            Button(action: {
+                isSecure.toggle()
+            }) {
+                Image(systemName: isSecure ? "eye.slash" : "eye")
+                    .foregroundColor(Color("ColorPrincipal"))
+            }
+            .padding(.trailing, 8)
+        }
+        .padding(.horizontal, 20)
     }
 }
 
-#Preview {
-    LoginView()
+struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isLoggedIn: Bool = false
+    @State private var showPassword: Bool = true
+    
+    let customGreen = Color(red: 162/255, green: 214/255, blue: 5/255)
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Bienvenido")
+                    .font(.largeTitle)
+                    .font(.system(size: 12, weight: .light, design: .serif))
+                    .bold() // Texto en negrita
+                    .foregroundColor(customGreen)
+                    .padding(.bottom, 0.5)
+                Text("Regístrate ahora")
+                    .font(.largeTitle)
+                    .bold() // Texto en negrita
+                    .foregroundColor(customGreen)
+                    .padding(.top, 20)
+                TextField("Nombre completo", text: $username)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(customGreen.opacity(0.2))
+                    .cornerRadius(20)
+                    .padding(.horizontal, 20)
+                    .autocapitalization(.none)
+                TextField("Nombre de usuario", text: $username)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(customGreen.opacity(0.2))
+                    .cornerRadius(20)
+                    .padding(.horizontal, 20)
+                    .autocapitalization(.none)
+                
+                PasswordTextField(text: $password, isSecure: $showPassword)
+        
+                Button(action: {
+                    isLoggedIn = true
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(customGreen)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 20)
+                }
+                
+                .padding(.top, 30)
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .opacity(isLoggedIn ? 0 : 1)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
