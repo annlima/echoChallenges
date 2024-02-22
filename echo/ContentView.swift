77874101e3f2104b1a988c7d67c162650d737d3c
Andy <alimablanca@gmail.com>
2024-02-22 09:35:29 -0600

    Initial Commit

---
+++ echo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 22/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echo/echoApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoApp.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct echoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
