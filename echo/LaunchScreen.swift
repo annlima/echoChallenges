9aaf3cadb0e9357f425be4fb3bfd6ed64692070a
Andy <alimablanca@gmail.com>
2024-02-22 09:48:26 -0600

    Primer commit

---
+++ echo/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 22/02/24.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
