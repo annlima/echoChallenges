7fa3b8cf0ad2618fd002ac31c3b50c490ccbe132
Andy <alimablanca@gmail.com>
2024-02-26 16:50:29 -0600

    Términos y condiciones y un poco de navegación

---
+++ echo/AuthAndOnboarding/TermsAndConditions.swift
@@ -0,0 +1,18 @@
+//
+//  TermsAndConditions.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 26/02/24.
+//
+
+import SwiftUI
+
+struct TermsAndConditions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsAndConditions()
+}
