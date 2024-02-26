4ff3db1548b1b6763322d4895212a5beaf2e9aad
Andy <alimablanca@gmail.com>
2024-02-26 12:47:37 -0600

    PasswordReset nueva vista y LaunchScreen mejoras

---
+++ echo/AuthAndOnboarding/PasswordReset.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordReset.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 26/02/24.
+//
+
+import SwiftUI
+
+struct PasswordReset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordReset()
+}
