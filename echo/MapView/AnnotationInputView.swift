8fd638c4ddda2dd3ecb350ac1658f8c89f0110c5
Dicka <dj_lezama@hotmail.com>
2024-03-04 15:45:59 -0600

    Cambios a la AnnotationInputForm y MapView

---
+++ echo/MapView/AnnotationInputView.swift
@@ -0,0 +1,18 @@
+//
+//  AnnotationInputView.swift
+//  echo
+//
+//  Created by Dicka J. Lezama on 04/03/24.
+//
+
+import SwiftUI
+
+struct AnnotationInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnnotationInputView()
+}
