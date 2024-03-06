5d6f7fe5797598372298d5afb6fb0e71c0481c9d
azu-any <azuany.mila@gmail.com>
2024-03-05 23:47:29 -0600

    posts en leader

---
+++ PhotoPicker.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPicker.swift
+//  echo
+//
+//  Created by Azuany Mila Cerón on 05/03/24.
+//
+
+import SwiftUI
+
+struct PhotoPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPicker()
+}
