af1466ec4c2d9db546f874ef026bc961023c0a53
azu-any <azuany.mila@gmail.com>
2024-03-06 15:47:33 -0600

    leader posts

--- echo/LeaderView/NewPostView.swift
+++ echo/LeaderView/NewPostView.swift
@@ -2,14 +2,83 @@
 //  NewPostView.swift
 //  echo
 //
-//  Created by Azuany Mila Cerón on 05/03/24.
+//  Created by Azuany Mila Cerón on 06/03/24.
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct NewPostView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var text: String = ""
+    @State private var photoItem: PhotosPickerItem?
+    @State private var photoImage: Image?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView{
+            
+            VStack(alignment: .center, spacing: 20) {
+                
+                Text("Nuevo Post")
+                    .font(.title)
+                    .bold()
+                
+                Divider()
+                
+                TextField("Escribe algo",
+                          text: $text,
+                          axis: .vertical
+                    )
+                
+                HStack() {
+                    
+                    Spacer()
+                    
+                    PhotosPicker(selection: $photoItem, matching: .images) {
+                        Label("", systemImage: "photo")
+                            .foregroundColor(.colorPrincipal)
+                            .font(.title)
+                    }
+                }
+                
+                photoImage?
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                
+            }
+            .onChange(of: photoItem) {
+                Task {
+                    if let loaded = try? await photoItem?.loadTransferable(type: Image.self) {
+                        photoImage = loaded
+                    } else {
+                        print("Failed")
+                    }
+                }
+            }
+            .padding()
+        }
+        .padding()
+        .overlay(
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+            }
+                .frame(width: 60, height: 60)
+                .background(Color.colorPrincipal)
+                .cornerRadius(30)
+                .shadow(radius: 10)
+                .offset(x: -25, y: 10),
+            alignment: .bottomTrailing
+        )
+        
     }
 }
 
--- echo/LeaderView/NewReunionView.swift
+++ echo/LeaderView/NewReunionView.swift
@@ -2,14 +2,68 @@
 //  NewReunionView.swift
 //  echo
 //
-//  Created by Azuany Mila Cerón on 05/03/24.
+//  Created by Azuany Mila Cerón on 06/03/24.
 //
 
 import SwiftUI
 
 struct NewReunionView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var topic: String = ""
+    @State private var results: String = ""
+    @State private var date: Date = Date.now
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView{
+            
+            VStack(spacing: 20) {
+                
+                Text("Nuevo Reporte de Junta")
+                    .font(.title)
+                    .bold()
+                
+                Divider()
+                
+                TextField("Tema",
+                          text: $topic,
+                          axis: .vertical
+                    )
+                
+                DatePicker(
+                        "Fecha",
+                        selection: $date,
+                        displayedComponents: [.date]
+                    )
+                    .datePickerStyle(.graphical)
+                
+                TextField("Resultados",
+                          text: $results,
+                          axis: .vertical
+                    )
+                
+                
+            }
+            .padding()
+        }
+        .padding()
+        .overlay(
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+            }
+                .frame(width: 60, height: 60)
+                .background(Color.colorPrincipal)
+                .cornerRadius(30)
+                .shadow(radius: 10)
+                .offset(x: -25, y: 10),
+            alignment: .bottomTrailing
+        )
     }
 }
 
