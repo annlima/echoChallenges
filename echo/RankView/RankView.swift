699849b7067cb1c574037b8dc98dad53d6f5e2d2
Andrea Lima <89811870+annlima@users.noreply.github.com>
2024-03-08 12:00:21 -0600

    rankview creation

---
+++ echo/RankView/RankView.swift
@@ -0,0 +1,18 @@
+//
+//  RankView.swift
+//  echo
+//
+//  Created by Andrea Lima Blanca on 08/03/24.
+//
+
+import SwiftUI
+
+struct RankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RankView()
+}
